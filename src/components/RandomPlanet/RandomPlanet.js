3f2a721ba8b0b6490d6563347044121e18174b77
romarios1987 <romios047@gmail.com>
2018-11-22 18:36:31 +0200

    get random planet

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -1,27 +1,67 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './RandomPlanet.sass'
+import SwapiService from "../../services/SwapiService";
 
-const RandomPlanet = () => {
-    return (
-        <div className='row'>
-            <div className="col-12">
-                <div className={classes.RandomPlanet}>
-                    <div className={classes["planet-image"]}>
-                        <img src="https://vignette.wikia.nocookie.net/uncyclopedia/images/3/3c/Naboo.png/revision/latest?cb=20081113082340" alt="NAme Planet"/>
-                    </div>
-                    <div className={classes["planet-info"]}>
-                        <h2>Naboo</h2>
 
-                        <ul>
-                            <li>PopulationPopulationPopulation</li>
-                            <li>Rotation</li>
-                            <li>Diameter</li>
-                        </ul>
+export default class RandomPlanet extends Component {
 
+
+    swapiService = new SwapiService();
+
+    state = {
+        id: null,
+        name: null,
+        population: null,
+        rotationPeriod: null,
+        diameter: null
+    };
+
+
+    constructor(){
+        super();
+        this.updatePlanet();
+    }
+
+
+    updatePlanet() {
+        const id = Math.floor(Math.random()*25) + 2;
+        this.swapiService
+            .getPlanet(id)
+            .then((planet) => {
+                this.setState({
+                    id,
+                    name: planet.name,
+                    population: planet.population,
+                    rotationPeriod: planet.rotation_period,
+                    diameter: planet.diameter
+                });
+            })
+    }
+
+    render() {
+        const {id, name, population, rotationPeriod, diameter} = this.state;
+        return (
+            <div className='row'>
+                <div className="col-12">
+                    <div className={classes.RandomPlanet}>
+                        <div className={classes["planet-image"]}>
+                            <img
+                                src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+                                alt={name}/>
+                        </div>
+                        <div className={classes["planet-info"]}>
+                            <h2>{name}</h2>
+
+                            <ul>
+                                <li>Population: <span>{population}</span></li>
+                                <li>Rotation: <span>{rotationPeriod}</span></li>
+                                <li>Diameter: <span>{diameter}</span></li>
+                            </ul>
+
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 };
-export default RandomPlanet;
