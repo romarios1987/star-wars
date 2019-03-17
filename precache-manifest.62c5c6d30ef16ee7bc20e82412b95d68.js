601e0c794d10a59cb85f89529b1d158b1e062307
romarios1987 <romios047@gmail.com>
2019-03-17 19:34:24 +0300

    Updates

--- precache-manifest.62c5c6d30ef16ee7bc20e82412b95d68.js
+++ precache-manifest.62c5c6d30ef16ee7bc20e82412b95d68.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/star-wars/static/js/runtime~main.1500f48b.js"
   },
   {
-    "revision": "71ae2936943fa0521ef4",
-    "url": "/star-wars/static/js/main.f05aeade.chunk.js"
+    "revision": "8e168de9d3b2f0b5afc5",
+    "url": "/star-wars/static/js/main.8c0a4ea1.chunk.js"
   },
   {
     "revision": "e5c4f39409fbdcb0b3f4",
     "url": "/star-wars/static/js/2.f469fc63.chunk.js"
   },
   {
-    "revision": "71ae2936943fa0521ef4",
+    "revision": "8e168de9d3b2f0b5afc5",
     "url": "/star-wars/static/css/main.7668f80b.chunk.css"
   },
   {
-    "revision": "e4473047c965804a368175b5577d9f5b",
+    "revision": "7a7de9c9c92921700a952d60c846f3ca",
     "url": "/star-wars/index.html"
   }
 ];
